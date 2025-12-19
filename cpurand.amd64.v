7966dcb92f549166c1367ed6558b1ea86221e395
Mike <tankf33der@disroot.org>
2025-12-19 13:36:04 +0200

    .

--- cpurand.amd64.v
+++ cpurand.amd64.v
@@ -1,6 +1,19 @@
 module cpurand
 
 pub fn cpu_support() bool {
-
-
+	mut result := false
+	asm amd64 {
+		// op dst, src
+		mov eax, 1
+		cpuid
+		bt ecx, 30
+		setc result
+		; =r (result)
+		; ; cc
+		  eax
+		  ebx
+		  ecx
+		  edx
+	}
+	return result
 }
